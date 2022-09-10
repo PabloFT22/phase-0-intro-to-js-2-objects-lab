63385b2447b08b1ade80b61880ac4ab3198e23be
Pablo Fuentes Tudela <pablo.fuentes.tudela@outlook.com>
2022-09-10 09:45:25 -0400

    commit tree 5ac2c5a393b563e8c2967bb2be1560d02460194a

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Write your solution in this file!
+const employee = {
+    name: "Pablo",
+    streetAddress: "123 Hartham Ct.",
+};
+function updateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+   const copyOfEmployees = { ...employee, streetAddress: value};
+   return copyOfEmployees;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, streetAddress, value) {
+    employee.streetAddress = value;
+    return employee;
+}
+function deleteFromEmployeeByKey(employee, key) {
+    const newEmployee = {...employee};
+    delete newEmployee[key];
+    return newEmployee;
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
